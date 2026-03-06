909267a1e607ecfba82865302a3e173705734046
Ihor843 <ihor.kunakh.kb.2022@lpnu.ua>
2026-03-07 00:41:24 +0200

    asdas

--- app/main.py
+++ app/main.py
@@ -1,8 +1,25 @@
+from __future__ import annotations
+
+
 class Person:
-    # write your code here
-    pass
+    people: dict[str, Person] = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
 
-def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+def create_person_list(people: list[dict]) -> list[Person]:
+    instances = [Person(p["name"], p["age"]) for p in people]
+
+    for person_dict in people:
+        name = person_dict["name"]
+        instance = Person.people[name]
+
+        if person_dict.get("wife") is not None:
+            instance.wife = Person.people[person_dict["wife"]]
+        elif person_dict.get("husband") is not None:
+            instance.husband = Person.people[person_dict["husband"]]
+
+    return instances
